8d81b6bf6c69db6d3c9ebef86243861de73a475f
Nishant Sanghai <nishantsanghai@gmail.com>
2021-06-22 18:21:45 +0530

    changed testfile

--- src/test/java/com/techprimers/testing/FizzBuzzTest.java
+++ src/test/java/com/techprimers/testing/FizzBuzzTest.java
@@ -21,7 +21,7 @@ class FizzBuzzTest {
     @DisplayName("Play FizzBuzz with number = 3")
     @Test
     public void testFizz() {
-        String fizzBuzz = fB.play(4);
+        String fizzBuzz = fB.play(3);
         Assertions.assertEquals(fizzBuzz, "Fizz");
     }
 
